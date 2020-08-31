83eeb7db227ee631e28c1f832d13e69046af9de0
Breno Abreu <brenogpa@gmail.com>
2020-08-31 20:56:19 -0300

    Estrutura sequencial (entrada, processamento e saída)
    Exercício 03

---
+++ exercicios_propostos_parte1/ex03/ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira 4 valores:");
+
+            int a = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int b = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int c = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int d = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            int dif = a * b - c * d;
+
+            Console.WriteLine("DIFERENÇA = " + dif);
+        }
+    }
+}

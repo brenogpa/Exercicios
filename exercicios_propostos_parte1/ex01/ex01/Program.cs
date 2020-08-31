a7182175c49b02636341d4fa331f638775b2be7c
Breno Abreu <brenogpa@gmail.com>
2020-08-31 20:16:41 -0300

    Estrutura sequencial (entrada, processamento e saída)

---
+++ exercicios_propostos_parte1/ex01/ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrada: ");
+            int valor1 = int.Parse(Console.ReadLine());
+            int valor2 = int.Parse(Console.ReadLine());
+
+            int soma = valor1 += valor2;
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}

302171bc4e3bd9ac1baa79bc2b1ec4c03f08dd21
Breno Abreu <brenogpa@gmail.com>
2020-09-01 20:53:35 -0300

    Estrutura Condicional
    Exercício 03

---
+++ exercicios_propostos_parte2/ex03/ex03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o valor (int) de A e B, respectivamente:");
+            string[] num = Console.ReadLine().Split();
+
+            int A = int.Parse(num[0]);
+            int B = int.Parse(num[1]);
+
+            if (A % B == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}

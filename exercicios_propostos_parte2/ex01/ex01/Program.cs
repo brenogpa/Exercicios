0f0f5471a647abdcbaf988a79fafc802d4f1f726
Breno Abreu <brenogpa@gmail.com>
2020-09-01 20:31:00 -0300

    Estrutura Condicional
    Exercício 01

---
+++ exercicios_propostos_parte2/ex01/ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("Numero Negativo");
+            }
+            else
+            {
+                Console.WriteLine("Numero Positivo");
+            }
+        }
+    }
+}

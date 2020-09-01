5d4a6e5b92bc3affd4cb37c6b7f8cd6dbd82198c
Breno Abreu <brenogpa@gmail.com>
2020-09-01 11:14:01 -0300

    Estrutura sequencial (entrada, processamento e saída)
    Exercício 04

---
+++ exercicios_propostos_parte1/ex04/ex04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o número do funcionário:");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o número de horas trabalhadas:");
+            int hour = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor que recebe por hora: ");
+            float valor = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float salary = valor * hour;
+
+            Console.WriteLine("Number: " + num +
+                "\nSalary: U$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

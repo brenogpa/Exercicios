a5adee4180a6fbdbcfb907fdda3d4f9ec5a3f197
Breno Abreu <brenogpa@gmail.com>
2020-09-01 20:40:23 -0300

    Estrutura Condicional
    Exercício 02

---
+++ exercicios_propostos_parte2/ex02/ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}

0a8dab33717f8f18d0f46bb131d80d8604522a1f
Breno Abreu <brenogpa@gmail.com>
2020-09-02 23:17:44 -0300

    Estrutura Condicional
    Exercício 04

---
+++ exercicios_propostos_parte2/ex04/ex04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a hora inicial e a hora final do jogo:");
+            string[] hour = Console.ReadLine().Split();
+
+            int start = int.Parse(hour[0]);
+            int end = int.Parse(hour[1]);
+
+            int time;
+
+            if (start < end)
+            {
+                time = end - start;
+            }
+            else
+            {
+                time = 24 - start + end;
+            }
+
+            Console.WriteLine("Duração do jogo: " + time + "h");
+        }
+    }
+}

b982dbfeaf5935bd678d1c45254b6ea66f1d542a
Breno Abreu <brenogpa@gmail.com>
2020-09-08 14:21:29 -0300

    Estrutura de Repetição (for)
    Exercício 01

---
+++ exercicios_propostos_parte4/ex01/ex01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+             X, se for o caso.
+            */
+
+            Console.Write("Digite um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= num; c++)
+            {
+                if (c % 2 != 0)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+    }
+}

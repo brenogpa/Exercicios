d7b86e811749e9de89de63dea6d78f255fed6cd2
Breno Abreu <brenogpa@gmail.com>
2020-09-09 12:45:52 -0300

    Estrutura de Repetição (for)
    Exercício 04

---
+++ exercicios_propostos_parte4/ex04/ex04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+            segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+            */
+
+            int n = int.Parse(Console.ReadLine());
+            double total;
+
+            for (int c = 1; c <= n; c++)
+            {
+                string[] valores = Console.ReadLine().Split();
+                double v0 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double v1 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+                if (v1 == 0)
+                {
+                    Console.WriteLine("Divisão Impossível");
+                }
+                else
+                {
+                    total = v0 / v1;
+                    Console.WriteLine(total.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}

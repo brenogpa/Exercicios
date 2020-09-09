33ed7f092f7eceea18a5ec57c5ee0a10195c94b6
Breno Abreu <brenogpa@gmail.com>
2020-09-09 13:24:39 -0300

    Estrutura de Repetição (for)
    Exercício 07

---
+++ exercicios_propostos_parte4/ex07/ex07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+                começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor.
+            */
+
+
+            Console.Write("Digite um número inteiro positivo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= n; c++)
+            {
+                Console.WriteLine($"{c} {c*c} {c*c*c} ");
+            }
+        }
+    }
+}

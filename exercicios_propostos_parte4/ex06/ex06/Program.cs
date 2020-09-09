dc2c67827afc70dea470fa26796667edd2b350c7
Breno Abreu <brenogpa@gmail.com>
2020-09-09 13:18:31 -0300

    Estrutura de Repetição (for)
    Exercício 06

---
+++ exercicios_propostos_parte4/ex06/ex06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Ler um número inteiro N e calcular todos os seus divisores.
+
+            Console.Write("Insira um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= n; c++)
+            {
+                if (n % c == 0)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+    }
+}

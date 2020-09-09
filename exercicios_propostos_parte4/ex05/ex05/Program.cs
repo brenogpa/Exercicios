51b2515f6fcb4715b19454bacb3bdd201a3506d5
Breno Abreu <brenogpa@gmail.com>
2020-09-09 13:12:06 -0300

    Estrutura de Repetição (for)
    Exercício 05

---
+++ exercicios_propostos_parte4/ex05/ex05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+                Lembrando que, por definição, fatorial de 0 é 1.
+            */
+            Console.Write("Fatorial \n Digite um valor: ");
+            int n = int.Parse(Console.ReadLine());
+            int fat = 1;
+
+            for (int c = 1; c <= n; c++)
+            {
+                fat *= c;
+            }
+
+            Console.WriteLine(fat);
+        }
+    }
+}

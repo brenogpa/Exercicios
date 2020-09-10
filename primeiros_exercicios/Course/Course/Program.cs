4f1186874f8a48064fcde4869bcb2b8d246d76fa
Breno Abreu <brenogpa@gmail.com>
2020-09-10 10:29:40 -0300

    Introdução à Programação Orientada a Objetos
    Criando classe

---
+++ primeiros_exercicios/Course/Course/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.Write("PRIMEIRA PESSOA \n Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.Write("SEGUNDA PESSOA \n Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {p1}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha: {p2}");
+            }
+        }
+    }
+}

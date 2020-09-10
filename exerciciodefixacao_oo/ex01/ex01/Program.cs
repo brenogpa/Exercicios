09cb54d116d1d4aac2797b76194fbf954305b143
Breno Abreu <brenogpa@gmail.com>
2020-09-10 17:43:58 -0300

    Orientação a Objetos (classe, atributos e métodos
    Exercício 01

---
+++ exerciciodefixacao_oo/ex01/ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.Write("Insira as medidas do Retângulo \n Largura: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write(" Altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA: " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture) +
+                "\nPERÍMETRO: " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture) +
+                "\nDIAGONAL: " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

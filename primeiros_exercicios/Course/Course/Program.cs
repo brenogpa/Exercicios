a6ea3ca22da3e0e6cabd7b7c41b0d6b5c0d53546
Breno Abreu <brenogpa@gmail.com>
2020-09-10 10:48:21 -0300

    Introdução à Programação Orientada a Objetos
    Criando classe 2

--- primeiros_exercicios/Course/Course/Program.cs
+++ primeiros_exercicios/Course/Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -6,6 +7,8 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            /*  EXERCÍCIO 01
+            
             Pessoa p1 = new Pessoa();
             Pessoa p2 = new Pessoa();
 
@@ -21,12 +24,31 @@ namespace Course
 
             if (p1.Idade > p2.Idade)
             {
-                Console.WriteLine($"Pessoa mais velha: {p1}");
+                Console.WriteLine($"Pessoa mais velha: {p1.Nome}");
             }
             else
             {
-                Console.WriteLine($"Pessoa mais velha: {p2}");
+                Console.WriteLine($"Pessoa mais velha: {p2.Nome}");
             }
+            */
+
+            //   EXERCÍCIO 02
+
+            Funcionario x1 = new Funcionario();
+            Funcionario x2 = new Funcionario();
+
+            Console.Write("PRIMEIRO FUNCIONÁRIO \n Nome: ");
+            x1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            x1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("SEGUNDO FUNCIONÁRIO \n Nome: ");
+            x2.Nome = Console.ReadLine();
+            Console.Write("Sálario: ");
+            x2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (x1.Salario + x2.Salario) / 2;
+            Console.WriteLine("Salário médio: " + media.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }

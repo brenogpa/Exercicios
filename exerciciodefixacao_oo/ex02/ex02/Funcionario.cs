db913ae3c1d2dac6dfef41786df931851478e09a
Breno Abreu <brenogpa@gmail.com>
2020-09-10 20:31:33 -0300

    Orientação a Objetos (classe, atributos e métodos)
    Exercício 02

---
+++ exerciciodefixacao_oo/ex02/ex02/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ex02
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return "Funcionário: " + Nome + ", R$ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

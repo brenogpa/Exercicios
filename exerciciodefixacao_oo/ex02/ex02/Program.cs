9b9f08fecad478a58a5af987b77da4dbcbc97d9c
Breno Abreu <brenogpa@gmail.com>
2020-09-10 20:31:33 -0300

    Orientação a Objetos (classe, atributos e métodos
    Exercício 02

---
+++ exerciciodefixacao_oo/ex02/ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: R$");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Imposto: R$");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(funcionario);
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.Write("|Dados Atualizados|\n" + funcionario);
+        }
+    }
+}

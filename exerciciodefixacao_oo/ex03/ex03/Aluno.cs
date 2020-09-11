a21656165664dadefa149bb96655807908425c64
Breno Abreu <brenogpa@gmail.com>
2020-09-10 21:46:15 -0300

    Orientação a Objetos (classe, atributos e métodos)
    Exercício 03

---
+++ exerciciodefixacao_oo/ex03/ex03/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace ex03
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public override string ToString()
+        {
+            if (NotaFinal() >= 60.0)
+            {
+                return "Nota final = " + NotaFinal().ToString("F2", CultureInfo.InvariantCulture)
+                    + "\nAPROVADO";
+            }
+            else
+            {
+                return "Nota final = " + NotaFinal().ToString("F2", CultureInfo.InvariantCulture)
+                    + "\nREPROVADO\nFALTARAM " + (60 - NotaFinal()) + " PONTOS";
+            }
+        }
+    }
+}
---
+++ exerciciodefixacao_oo/ex03/ex03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+             (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+             ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam
+             para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver
+             este problema.
+             */
+
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(aluno);
+        }
+    }
+}

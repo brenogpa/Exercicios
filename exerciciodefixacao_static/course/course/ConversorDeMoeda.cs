be6ffada60cb73eda63764eff6d58701f2dcf43c
Breno Abreu <brenogpa@gmail.com>
2020-09-16 21:05:39 -0300

    Membros estáticos
    Exercício de Fixação

---
+++ exerciciodefixacao_static/course/course/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace course
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 0.06;
+
+        public static double ValorTotal(double c, double q)
+        {
+            double total = c * q;
+            return total + total * Iof;
+        }
+    }
+}
---
+++ exerciciodefixacao_static/course/course/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**  Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
+                 uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+                 que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
+                 para ser responsável pelos cálculos.
+            **/
+            Console.Write("Qual é a cotação do dólar?: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar?: ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = ConversorDeMoeda.ValorTotal(cotacao, qtd);
+
+            Console.Write("Valor a ser pago em reais: R$ " + total.ToString("F2", CultureInfo.InvariantCulture)) ;
+        }
+    }
+}

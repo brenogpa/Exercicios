13747fbd8c3d5d8e707fed0028c8015985e93eda
Breno Abreu <brenogpa@gmail.com>
2020-09-21 16:37:57 -0300

    Arrays ( vectors )
    Exercício de Fixação

---
+++ exerciciodefixacao_vetores/Course/Course/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            A dona de um pensionato possui dez quartos para alugar para estudantes,
+            sendo esses quartos identificados pelos números 0 a 9.
+            Quando um estudante deseja alugar um quarto, deve-se registrar o nome
+            e email deste estudante.
+            Fazer um programa que inicie com todos os dez quartos vazios, e depois
+            leia uma quantidade N representando o número de estudantes que vão
+            alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos
+            N estudantes. Para cada registro de aluguel, informar o nome e email do
+            estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
+            que seja escolhido um quarto vago. Ao final, seu programa deve imprimir
+            um relatório de todas ocupações do pensionato, por ordem de quarto,
+            conforme exemplo.
+            */
+
+
+            Console.Write("How many rooms will be rented?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Rent[] vect = new Rent[10];
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Rent # {i}");
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                vect[room] = new Rent(name, email);
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ exerciciodefixacao_vetores/Course/Course/Rent.cs
@@ -0,0 +1,19 @@
+﻿namespace Course
+{
+    class Rent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Rent(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}

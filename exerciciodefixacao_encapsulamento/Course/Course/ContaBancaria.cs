f241fcbdbe344052bfb42b100f49bcb641777210
Breno Abreu <brenogpa@gmail.com>
2020-09-20 23:52:22 -0300

    Construtores, this, sobrecarga, encapsulamento
    Exercício de Fixação

---
+++ exerciciodefixacao_encapsulamento/Course/Course/ContaBancaria.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public override string ToString()
+        {
+            return " Conta " + Numero + 
+                ", Titular: " + Titular + 
+                ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public double Deposito(double deposito)
+        {
+            return Saldo += deposito;
+        }
+
+        public double Saque(double saque)
+        {
+            return Saldo -= saque + 5.00;
+        }
+    }
+}
